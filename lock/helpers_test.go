227728c2e87352d177539551a613b8d765c05efd
Jathurchan Selvakumar <admin@jathurchan.com>
2025-05-22 02:08:11 +0200

    test: Added tests for lockManager

--- lock/helpers_test.go
+++ lock/helpers_test.go
@@ -11,13 +11,21 @@ type mockClock struct {
 }
 
 func newMockClock() *mockClock {
-	return &mockClock{currentTime: time.Date(2025, 1, 1, 12, 0, 0, 0, time.UTC)}
+	return &mockClock{currentTime: time.Now()}
 }
 
-func (m *mockClock) Now() time.Time                         { return m.currentTime }
-func (m *mockClock) Since(t time.Time) time.Duration        { return m.currentTime.Sub(t) }
-func (m *mockClock) After(d time.Duration) <-chan time.Time { return nil }
-func (m *mockClock) NewTicker(d time.Duration) raft.Ticker  { return nil }
-func (m *mockClock) NewTimer(d time.Duration) raft.Timer    { return nil }
-func (m *mockClock) Sleep(d time.Duration)                  {}
-func (m *mockClock) Advance(d time.Duration)                { m.currentTime = m.currentTime.Add(d) }
+func (m *mockClock) Now() time.Time                  { return m.currentTime }
+func (m *mockClock) Since(t time.Time) time.Duration { return m.currentTime.Sub(t) }
+func (m *mockClock) After(d time.Duration) <-chan time.Time {
+	ch := make(chan time.Time, 1)
+	ch <- m.currentTime.Add(d)
+	return ch
+}
+func (m *mockClock) NewTicker(d time.Duration) raft.Ticker { return nil }
+func (m *mockClock) NewTimer(d time.Duration) raft.Timer   { return nil }
+func (m *mockClock) Sleep(d time.Duration) {
+	m.currentTime = m.currentTime.Add(d)
+}
+func (m *mockClock) Advance(d time.Duration) {
+	m.currentTime = m.currentTime.Add(d)
+}
